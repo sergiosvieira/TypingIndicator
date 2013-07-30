a770b9e42dad6483d858d610d823a7615cda92e8
Sérgio Vieira <sergiosvieira@gmail.com>
2013-07-30 11:07:50 -0300

    Initial Commit

---
+++ TypingIndicator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TypingIndicator
+//
+//  Created by Paulo Pinheiro on 7/30/13.
+//  Copyright (c) 2013 Bravo Inovação. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TypingIndicator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TypingIndicator
+//
+//  Created by Paulo Pinheiro on 7/30/13.
+//  Copyright (c) 2013 Bravo Inovação. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
